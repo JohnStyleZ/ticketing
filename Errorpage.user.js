31556381f442d68be1dac0132a71d0faa8d237d0
John Wu <33677676+JohnStyleZ@users.noreply.github.com>
2024-05-07 22:13:10 -0700

    Create Errorpage.user.js

---
+++ Errorpage.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Reload Page on Error
+// @version      1.0
+// @author       JohnStyleZ
+// @namespace    https://your.namespace.com
+// @match        https://*.hkticketing.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    // Function to check the page's server response status
+    function checkServerStatus() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 504) {
+                    // Redirect to a specific URL
+                    window.location.href = "https://queue.hkticketing.com/hotshow.html";
+                }
+            }
+        };
+        // Send a HEAD request to the current page
+        xhttp.open("HEAD", window.location.href, true);
+        xhttp.send();
+    }
+
+    // Check for server status periodically
+    setInterval(checkServerStatus, 5000); // Check every 5 seconds
+
+})();
